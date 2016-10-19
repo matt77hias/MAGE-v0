2b9228773f69d1ce44e53e7d9066f537445db86b
ALIEN <matthias.moulin@gmail.com>
2016-10-19 11:07:27 +0200

    Added docu

--- MAGE/MAGE/src/memory/allocation.hpp
+++ MAGE/MAGE/src/memory/allocation.hpp
@@ -11,45 +11,37 @@ namespace mage {
 	// Declarations and Definitions
 	//-----------------------------------------------------------------------------
 
-	/// <summary>
-	/// Allocates memory on an alignment boundary of 64 bytes.
-	/// </summary>
-	/// <param name="size">
-	/// The requested size in bytes to allocate in memory.
-	/// </param>
-	/// <returns>
-	/// A pointer to the memory block that was allocated or NULL if the operation failed.
-	/// The pointer is a multiple of the alignment of 64 bytes.
-	/// </returns>
+	/**
+	 Allocates memory on an alignment boundary of 64 bytes.
+	 
+	 @param[in]		size	The requested size in bytes to allocate in memory.
+	 @return				@c NULL if the allocation failed.
+	 @return				A pointer to the memory block that was allocated.
+	 						The pointer is a multiple of the alignment of 64 bytes.
+	 */
 	inline void *AllocAligned(size_t size) {
 		return _aligned_malloc(size, MAGE_L1_CACHE_LINE_SIZE);
 	}
 
-	/// <summary>
-	/// Allocates memory on an alignment boundary of 64 bytes.
-	/// </summary>
-	/// <typeparam name='T'>
-	/// The type of objects to allocate in memory.
-	/// </typeparam>
-	/// <param name="count">
-	/// The number of objects of type T to allocate in memory.
-	/// </param>
-	/// <returns>
-	/// A pointer to the memory block that was allocated or NULL if the operation failed.
-	/// The pointer is a multiple of the alignment of 64 bytes.
-	/// </returns>
+	/**
+	 Allocates memory on an alignment boundary of 64 bytes.
+
+	 @tparam		T		The type of objects to allocate in memory.
+	 @param[in]		count	The number of objects of type @c T to allocate in memory.
+	 @return				@c NULL if the allocation failed.
+	 @return				A pointer to the memory block that was allocated.
+	 						The pointer is a multiple of the alignment of 64 bytes.
+	*/
 	template <typename T>
 	inline T *AllocAligned(uint32_t count) {
 		return (T *)AllocAligned(count * sizeof(T));
 	}
 
-	/// <summary>
-	/// Frees a block of memory that was allocated with AllocAligned.
-	/// </summary>
-	/// <param name="ptr">
-	/// A pointer to the memory block that was allocated.
-	/// This function returns if <paramref name="ptr"/> points to NULL.
-	/// </param>
+	/**
+	 Frees a block of memory that was allocated with {@link mage::AllocAligned(size_t)} or {@link mage::AllocAligned<T>(uint32_t)}.
+	
+	 @param[in]		ptr		A pointer to the memory block that was allocated.
+	*/
 	inline void FreeAligned(void *ptr) {
 		if (!ptr) {
 			return;

2f5b83d6160aee3476c6c944d9a2a3072975c0ea
ALIEN <matthias.moulin@gmail.com>
2016-10-31 10:17:10 +0100

    Renamed scripts to .mage files

--- MAGE/FPS/src/core/FPS.cpp
+++ MAGE/FPS/src/core/FPS.cpp
@@ -7,11 +7,13 @@ using namespace mage;
 
 class TestState : public State {
 	virtual void Update(double elapsed_time) {
+		(void) elapsed_time;
+
 		if (g_engine->GetInput()->GetKeyPress(DIK_Q)) {
 			//PostQuitMessage(0);
 
-			VariableScript s("script_test.txt", "C:/Users/Matthias/Documents/Visual Studio 2015/Projects/MAGE/MAGE/FPS/script/");
-			s.SaveScript("C:/Users/Matthias/Documents/Visual Studio 2015/Projects/MAGE/MAGE/FPS/script/output.txt");
+			VariableScript s("script_test.mage", "C:/Users/Matthias/Documents/Visual Studio 2015/Projects/MAGE/MAGE/FPS/script/");
+			s.SaveScript("C:/Users/Matthias/Documents/Visual Studio 2015/Projects/MAGE/MAGE/FPS/script/output.mage");
 				
 		}
 	};

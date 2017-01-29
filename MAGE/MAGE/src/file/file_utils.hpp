6e66a408eb9cbfb9a0dfbe8547dd432271badffe
ALIEN <matthias.moulin@gmail.com>
2017-01-29 22:29:20 +0100

    Updated documentation

--- MAGE/MAGE/src/file/file_utils.hpp
+++ MAGE/MAGE/src/file/file_utils.hpp
@@ -18,39 +18,99 @@ namespace mage {
 //-----------------------------------------------------------------------------
 namespace mage {
 
+	/**
+	 Returns the filename of the given file.
+
+	 @param[in]		path
+					A reference to the path of the file.
+	 @param[in]		name
+					A reference to the name of the file.
+	 @return		The filename of the given file.
+	 */
 	inline string GetFilename(const string &path, const string &name) {
 		return path + name;
 	}
 
+	/**
+	 Returns the filename of the given file.
+
+	 @param[in]		path
+					A reference to the path of the file.
+	 @param[in]		name
+					A reference to the name of the file.
+	 @return		The filename of the given file.
+	 */
 	inline wstring GetFilename(const wstring &path, const wstring &name) {
 		return path + name;
 	}
 
+	/**
+	 Returns the extension of the given file.
+
+	 @param[in]		fname
+					A reference to the filename of the file.
+	 @return		The extension of the given file.
+	 */
 	inline string GetFileExtension(const string &fname) {
 		const size_t begin_pos = fname.find_last_of(".");
 		return fname.substr(begin_pos + 1);
 	}
 
+	/**
+	 Returns the extension of the given file.
+
+	 @param[in]		fname
+					A reference to the filename of the file.
+	 @return		The extension of the given file.
+	 */
 	inline wstring GetFileExtension(const wstring &fname) {
 		const size_t begin_pos = fname.find_last_of(L".");
 		return fname.substr(begin_pos + 1);
 	}
 
+	/**
+	 Returns the name of the given file.
+
+	 @param[in]		fname
+					A reference to the filename of the file.
+	 @return		The name of the given file.
+	 */
 	inline string GetFileName(const string &fname) {
 		const size_t begin_pos = fname.find_last_of("/");
 		return fname.substr(begin_pos + 1);
 	}
 
+	/**
+	 Returns the name of the given file.
+
+	 @param[in]		fname
+					A reference to the filename of the file.
+	 @return		The name of the given file.
+	 */
 	inline wstring GetFileName(const wstring &fname) {
 		const size_t begin_pos = fname.find_last_of(L"/");
 		return fname.substr(begin_pos + 1);
 	}
 
+	/**
+	 Returns the path of the given file.
+
+	 @param[in]		fname
+					A reference to the filename of the file.
+	 @return		The path of the given file.
+	 */
 	inline string GetPathName(const string &fname) {
 		const size_t end_pos = fname.find_last_of("/");
 		return fname.substr(0, end_pos + 1);
 	}
 
+	/**
+	 Returns the path of the given file.
+
+	 @param[in]		fname
+					A reference to the filename of the file.
+	 @return		The path of the given file.
+	 */
 	inline wstring GetPathName(const wstring &fname) {
 		const size_t end_pos = fname.find_last_of(L"/");
 		return fname.substr(0, end_pos + 1);

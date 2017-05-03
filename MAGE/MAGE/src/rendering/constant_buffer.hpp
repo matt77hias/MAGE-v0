64a251c54f668dd8c2f5f3adbc6b14f6e14c6504
REDDRAGON <matthias.moulin@gmail.com>
2017-05-03 19:12:09 +0200

    Updated documentation

--- MAGE/MAGE/src/rendering/constant_buffer.hpp
+++ MAGE/MAGE/src/rendering/constant_buffer.hpp
@@ -15,6 +15,12 @@
 //-----------------------------------------------------------------------------
 namespace mage {
 
+	/**
+	 A class of constant buffers (for binding buffers to the rendering pipeline).
+
+	 @tparam		DataT
+					The data type.
+	 */
 	template< typename DataT >
 	struct ConstantBuffer final {
 
@@ -24,29 +30,101 @@ namespace mage {
 		// Constructors and Destructors
 		//---------------------------------------------------------------------
 
+		/**
+		 Constructs a constant buffer.
+
+		 @pre			@a device is not equal to @c nullptr.
+		 @pre			@a device_context is not equal to @c nullptr.
+		 @param[in]		device
+						A pointer to the device.
+		 @param[in]		device_context
+						A pointer to the device context.
+		 */
 		explicit ConstantBuffer(ID3D11Device2 *device, ID3D11DeviceContext2 *device_context);
+		
+		/**
+		 Constructs a constant buffer from the given constant buffer.
+
+		 @param[in]		buffer
+						A reference to the constant buffer to copy.
+		 */
 		ConstantBuffer(const ConstantBuffer &buffer) = delete;
+
+		/**
+		 Constructs a constant buffer by moving the given constant buffer.
+
+		 @param[in]		buffer
+						A reference to the constant buffer to move.
+		 */
 		ConstantBuffer(ConstantBuffer &&buffer) = default;
+		
+		/**
+		 Destructs this constant buffer.
+		 */
 		~ConstantBuffer() = default;
 
 		//---------------------------------------------------------------------
 		// Assignment Operators
 		//---------------------------------------------------------------------	
 
+		/**
+		 Copies the given constant buffer to this constant buffer.
+
+		 @param[in]		buffer
+						A reference to the constant buffer to copy.
+		 @return		A reference to the copy of the given constant buffer
+						(i.e. this constant buffer).
+		 */
 		ConstantBuffer &operator=(const ConstantBuffer &buffer) = delete;
+
+		/**
+		 Moves the given constant buffer to this constant buffer.
+
+		 @param[in]		buffer
+						A reference to the constant buffer to move.
+		 @return		A reference to the copy of the given constant buffer
+						(i.e. this constant buffer).
+		 */
 		ConstantBuffer &operator=(ConstantBuffer &&buffer) = delete;
 
 		//---------------------------------------------------------------------
 		// Member Methods
 		//---------------------------------------------------------------------
 
+		/**
+		 Updates the data of constant buffer with the given data.
+
+		 @param[in]		data
+						A reference to the data.
+		 */
 		void UpdateData(const DataT &data) const;
+		
+		/**
+		 Returns the buffer resource of this constant buffer.
+
+		 @return		A pointer to the buffer resource 
+						of this constant buffer.
+		 */
 		ID3D11Buffer *Get() const {
 			return m_buffer.Get();
 		}
+		
+		/**
+		 Returns the address of the buffer resource of this constant buffer.
+
+		 @return		A pointer to a pointer to the buffer resource 
+						of this constant buffer.
+		 */
 		ID3D11Buffer * const *GetAddressOf() const {
 			return m_buffer.GetAddressOf();
 		}
+		
+		/**
+		 Returns the address of the buffer resource of this constant buffer.
+
+		 @return		A pointer to a pointer to the buffer resource 
+						of this constant buffer.
+		 */
 		ID3D11Buffer **GetAddressOf() {
 			return m_buffer.GetAddressOf();
 		}
@@ -56,17 +134,34 @@ namespace mage {
 		//---------------------------------------------------------------------
 		// Member Methods
 		//---------------------------------------------------------------------
+		
+		/**
+		 Sets up the resource buffer of this constant buffer.
+
+		 @throws		FormattedException
+						Failed to setup this constant buffer.
+		 */
 		void SetupConstantBuffer();
 
 		//---------------------------------------------------------------------
 		// Member Variables
 		//---------------------------------------------------------------------
 
+		/**
+		 A pointer to the device of this constant buffer.
+		 */
 		ID3D11Device2 * const m_device;
+
+		/**
+		 A pointer to the device context of this constant buffer.
+		 */
 		ID3D11DeviceContext2 * const m_device_context;
+
+		/**
+		 A pointer to the buffer resource of this constant buffer.
+		 */
 		ComPtr< ID3D11Buffer > m_buffer;
 	};
-
 }
 
 //-----------------------------------------------------------------------------
--- MAGE/MAGE/src/rendering/structured_buffer.hpp
+++ MAGE/MAGE/src/rendering/structured_buffer.hpp
@@ -16,6 +16,12 @@
 //-----------------------------------------------------------------------------
 namespace mage {
 
+	/**
+	 A class of structured buffers (for binding arrays of buffers to the rendering pipeline).
+
+	 @tparam		DataT
+					The data type.
+	 */
 	template< typename DataT >
 	struct StructuredBuffer final {
 
@@ -25,33 +31,104 @@ namespace mage {
 		// Constructors and Destructors
 		//---------------------------------------------------------------------
 
+		/**
+		 Constructs a structured buffer.
+
+		 @pre			@a device is not equal to @c nullptr.
+		 @pre			@a device_context is not equal to @c nullptr.
+		 @param[in]		device
+						A pointer to the device.
+		 @param[in]		device_context
+						A pointer to the device context.
+		 @param[in]		nb_data_elements
+						The initial number of slots for storing data elements to provide.
+		 */
 		explicit StructuredBuffer(ID3D11Device2 *device, ID3D11DeviceContext2 *device_context, 
 			size_t nb_initial_data_elements);
+		
+		/**
+		 Constructs a structured buffer from the given structured buffer.
+
+		 @param[in]		buffer
+						A reference to the structured buffer to copy.
+		 */
 		StructuredBuffer(const StructuredBuffer &buffer) = delete;
+
+		/**
+		 Constructs a structured buffer by moving the given structured buffer.
+
+		 @param[in]		buffer
+						A reference to the structured buffer to move.
+		 */
 		StructuredBuffer(StructuredBuffer &&buffer) = default;
+
+		/**
+		 Destructs this structured buffer.
+		 */
 		~StructuredBuffer() = default;
 
 		//---------------------------------------------------------------------
 		// Assignment Operators
 		//---------------------------------------------------------------------	
 
+		/**
+		 Copies the given structured buffer to this structured buffer.
+
+		 @param[in]		buffer
+						A reference to the structured buffer to copy.
+		 @return		A reference to the copy of the given structured buffer
+						(i.e. this structured buffer).
+		 */
 		StructuredBuffer &operator=(const StructuredBuffer &buffer) = delete;
+
+		/**
+		 Moves the given structured buffer to this structured buffer.
+
+		 @param[in]		buffer
+						A reference to the structured buffer to move.
+		 @return		A reference to the copy of the given structured buffer
+						(i.e. this structured buffer).
+		 */
 		StructuredBuffer &operator=(StructuredBuffer &&buffer) = delete;
 
 		//---------------------------------------------------------------------
 		// Member Methods
 		//---------------------------------------------------------------------
 
-		size_t GetNumberOfDataElements() const {
-			return m_nb_data_elements;
-		}
+		/**
+		 Updates the data of structured buffer with the given data.
+
+		 @param[in]		data
+						A reference to a vector with the data elements.
+		 */
 		void UpdateData(const vector< DataT > &data) const;
+
+		/**
+		 Returns the shader resource view of this structured buffer.
+
+		 @return		A pointer to the shader resource view
+						of this structured buffer.
+		 */
 		ID3D11ShaderResourceView *Get() const {
 			return m_shader_resource_view.Get();
 		}
+
+		/**
+		 Returns the address of the shader resource view of this structured buffer.
+
+		 @return		A pointer to a pointer to the shader resource view
+						of this structured buffer.
+		 */
 		ID3D11ShaderResourceView * const *GetAddressOf() const {
 			return m_shader_resource_view.GetAddressOf();
 		}
+
+		/**
+		 Returns the address of the shader resource view of this structured buffer.
+
+		 @return		A pointer to a pointer to the shader resource view
+						of this structured buffer.
+		 */
 		ID3D11ShaderResourceView **GetAddressOf() {
 			return m_shader_resource_view.GetAddressOf();
 		}
@@ -61,19 +138,47 @@ namespace mage {
 		//---------------------------------------------------------------------
 		// Member Methods
 		//---------------------------------------------------------------------
+		
+		/**
+		 Sets up the resource buffer and shader resource view of this structured buffer.
+
+		 @param[in]		nb_data_elements
+						The number of slots for storing data elements to provide.
+		 @throws		FormattedException
+						Failed to setup this structured buffer.
+		 */
 		void SetupStructuredBuffer(size_t nb_data_elements) const;
 
 		//---------------------------------------------------------------------
 		// Member Variables
 		//---------------------------------------------------------------------
 
+		/**
+		 A pointer to the device of this structured buffer.
+		 */
 		ID3D11Device2 * const m_device;
+
+		/**
+		 A pointer to the device context of this structured buffer.
+		 */
 		ID3D11DeviceContext2 * const m_device_context;
+
+		/**
+		 A pointer to the buffer resource of this structured buffer.
+		 */
 		mutable ComPtr< ID3D11Buffer > m_buffer;
+
+		/**
+		 The number of slots available for storing data elements
+		 in the current buffer resource of this structured buffer.
+		 */
 		mutable size_t m_nb_data_elements;
+
+		/**
+		 A pointer to the shader resource view of this structured buffer.
+		 */
 		mutable ComPtr< ID3D11ShaderResourceView > m_shader_resource_view;
 	};
-
 }
 
 //-----------------------------------------------------------------------------

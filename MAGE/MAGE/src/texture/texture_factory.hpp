11258963061adf0f80c5ff30ed5e89def862f8ec
REDDRAGON <matthias.moulin@gmail.com>
2017-05-07 14:41:15 +0200

    Added texture_factory

---
+++ MAGE/MAGE/src/texture/texture_factory.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+//-----------------------------------------------------------------------------
+// Engine Includes
+//-----------------------------------------------------------------------------
+#pragma region
+
+#include "texture\texture.hpp"
+
+#pragma endregion
+
+//-----------------------------------------------------------------------------
+// Engine Declarations
+//-----------------------------------------------------------------------------
+namespace mage {
+
+	/**
+	 Creates a black texture.
+
+	 @pre			The current engine must be loaded.
+	 @return		A pointer to the texture.
+	 @throws		FormattedException
+					Failed to create the texture.
+	 */
+	inline SharedPtr< Texture > CreateBlackTexture() {
+		return CreateTexture(L"assets/sprites/black.dds");
+	}
+
+	/**
+	 Creates a white texture.
+
+	 @pre			The current engine must be loaded.
+	 @return		A pointer to the texture.
+	 @throws		FormattedException
+					Failed to create the texture.
+	 */
+	inline SharedPtr< Texture > CreateWhiteTexture() {
+		return CreateTexture(L"assets/sprites/white.dds");
+	}
+}

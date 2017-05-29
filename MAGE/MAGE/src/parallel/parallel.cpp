dfae9a6c128d9042939a3344feec7ac8d2e42c45
REDDRAGON <matthias.moulin@gmail.com>
2017-05-29 19:02:34 +0200

    Added method for retrieving physical cores

--- MAGE/MAGE/src/parallel/parallel.cpp
+++ MAGE/MAGE/src/parallel/parallel.cpp
@@ -3,8 +3,9 @@
 //-----------------------------------------------------------------------------
 #pragma region
 
-#include "platform\windows.hpp"
+#include "memory\memory.hpp"
 #include "parallel\parallel.hpp"
+#include "logging\error.hpp"
 
 #pragma endregion
 
@@ -13,7 +14,37 @@
 //-----------------------------------------------------------------------------
 namespace mage {
 
+	size_t NumberOfPhysicalCores() noexcept {
+
+		DWORD length = 0;
+		const BOOL result_first = GetLogicalProcessorInformationEx(RelationProcessorCore, nullptr, &length);
+		
+		Assert(result_first == FALSE);
+		Assert(GetLastError() == ERROR_INSUFFICIENT_BUFFER);
+
+		UniquePtr< uint8_t[] > buffer(new uint8_t[length]);
+		const PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX info = 
+			reinterpret_cast< PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX >(buffer.get());
+
+		const BOOL result_second = GetLogicalProcessorInformationEx(RelationProcessorCore, info, &length);
+
+		Assert(result_second == TRUE);
+
+		size_t nb_physical_cores = 0;
+		size_t offset = 0;
+		do {
+			const PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX current_info =
+				reinterpret_cast< PSYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX >(buffer.get() + offset);
+			offset += current_info->Size;
+			++nb_physical_cores;
+		} while (offset < length);
+		
+		return nb_physical_cores;
+	}
+
 	size_t NumberOfSystemCores() noexcept {
+		NumberOfPhysicalCores();
+
 		// Structure containing information about the current computer system. 
 		SYSTEM_INFO system_info;
 		ZeroMemory(&system_info, sizeof(system_info));

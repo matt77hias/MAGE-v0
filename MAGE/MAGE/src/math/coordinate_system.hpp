1166f78c410551b0791f4688ebdfb94d1c9bdae1
ALIEN <matthias.moulin@gmail.com>
2017-07-25 17:02:17 +0200

    Added multiple methods for calculating 3D orthonormal bases

--- MAGE/MAGE/src/math/coordinate_system.hpp
+++ MAGE/MAGE/src/math/coordinate_system.hpp
@@ -15,6 +15,92 @@
 //-----------------------------------------------------------------------------
 namespace mage {
 
+	/**
+	 Calculates an orthonormal basis from a given unit vector
+	 with the method of Hughes and Möller.
+
+	 @pre			The given vector is normalized.
+	 @param[in]		n
+					The first basis vector of the orthonormal basis.
+	 @param[out]	b1
+					The second basis vector of the orthonormal basis.
+	 @param[out]	b2
+					The third basis vector of the orthonormal basis.
+	 */
+	inline void OrthonormalBasis_HughesMoller(FXMVECTOR n, XMVECTOR &b1, XMVECTOR &b2) noexcept {
+		const XMVECTOR u = (fabs(XMVectorGetX(n)) > fabs(XMVectorGetZ(n)))
+			? XMVectorSet(-XMVectorGetY(n), XMVectorGetX(n), 0.0f, 0.0f)
+			: XMVectorSet(0.0f, -XMVectorGetZ(n), XMVectorGetY(n), 0.0f);
+		b2 = XMVector3Normalize(u);
+		b1 = XMVector3Cross(b2, n);
+	}
+
+	/**
+	 Calculates an orthonormal basis from a given unit vector
+	 with the method of Frisvad.
+
+	 @pre			The given vector is normalized.
+	 @param[in]		n
+					The first basis vector of the orthonormal basis.
+	 @param[out]	b1
+					The second basis vector of the orthonormal basis.
+	 @param[out]	b2
+					The third basis vector of the orthonormal basis.
+	 */
+	inline void OrthonormalBasis_Frisvad(FXMVECTOR n, XMVECTOR &b1, XMVECTOR &b2) noexcept {
+		XMFLOAT3 nf;
+		XMStoreFloat3(&nf, n);
+
+		if (nf.z < -0.9999999f) {
+			b1 = XMVectorSet( 0.0f, -1.0f, 0.0f, 0.0f);
+			b2 = XMVectorSet(-1.0f,  0.0f, 0.0f, 0.0f);
+			return;
+		}
+
+		const float a = 1.0f / (1.0f + nf.z);
+		const float b = -nf.x * nf.y * a;
+		b1 = XMVectorSet(1.0f - nf.x * nf.x * a, b, -nf.x, 0.0f);
+		b2 = XMVectorSet(b, 1.0f - nf.y * nf.y * a, -nf.y, 0.0f);
+	}
+
+	/**
+	 Calculates an orthonormal basis from a given unit vector
+	 with the method of Duff, Burgess, Christensen, Hery, Kensler, Liani and Villemin.
+
+	 @pre			The given vector is normalized.
+	 @param[in]		n
+					The first basis vector of the orthonormal basis.
+	 @param[out]	b1
+					The second basis vector of the orthonormal basis.
+	 @param[out]	b2
+					The third basis vector of the orthonormal basis.
+	 */
+	inline void OrthonormalBasis_Duff(FXMVECTOR n, XMVECTOR &b1, XMVECTOR &b2) noexcept {
+		XMFLOAT3 nf;
+		XMStoreFloat3(&nf, n);
+
+		const float sign = copysignf(1.0f, nf.z);
+		const float a = -1.0f / (sign + nf.z);
+		const float b = nf.x * nf.y * a;
+		b1 = XMVectorSet(1.0f + sign * nf.x * nf.x * a, sign * b, -sign * nf.x, 0.0f);
+		b2 = XMVectorSet(b, sign + nf.y * nf.y * a, -nf.y, 0.0f);
+	}
+
+	/**
+	 Calculates an orthonormal basis from a given unit vector.
+
+	 @pre			The given vector is normalized.
+	 @param[in]		n
+					The first basis vector of the orthonormal basis.
+	 @param[out]	b1
+					The second basis vector of the orthonormal basis.
+	 @param[out]	b2
+					The third basis vector of the orthonormal basis.
+	 */
+	inline void OrthonormalBasis(FXMVECTOR n, XMVECTOR &b1, XMVECTOR &b2) noexcept {
+		OrthonormalBasis_Duff(n, b1, b2);
+	}
+
 	//-------------------------------------------------------------------------
 	// CartesianAxesSystem
 	//-------------------------------------------------------------------------
@@ -47,11 +133,7 @@ namespace mage {
 		 */
 		explicit CartesianAxesSystem(FXMVECTOR x) 
 			: m_x(x) {
-			const XMVECTOR u = (fabs(XMVectorGetX(m_x)) > 0.0f) ?
-								XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f) : 
-								XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
-			m_y = XMVector3Cross(m_x, u);
-			m_z = XMVector3Cross(m_x, m_y);
+			OrthonormalBasis(m_x, m_y, m_z);
 		}
 
 		/**

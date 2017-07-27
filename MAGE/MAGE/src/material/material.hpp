b34304830dfbd7690bbf55f5602e40afa143cbfc
REDDRAGON <matthias.moulin@gmail.com>
2017-07-27 18:55:40 +0200

    UpdateTransparency in Material::Material

--- MAGE/MAGE/src/material/material.hpp
+++ MAGE/MAGE/src/material/material.hpp
@@ -43,14 +43,16 @@ namespace mage {
 						A reference to the name of the material.
 		 */
 		explicit Material(const string &name)
-			: m_name(name), m_transmission_filter(),
+			: m_name(name), m_transparent(), m_transmission_filter(),
 			m_ambient_reflectivity(), m_ambient_reflectivity_texture(),
 			m_diffuse_reflectivity(), m_diffuse_reflectivity_texture(),
 			m_specular_reflectivity(), m_specular_reflectivity_texture(),
 			m_specular_exponent(1.0f), m_specular_exponent_texture(),
 			m_dissolve(1.0f), m_index_of_refraction(1.0f), 
 			m_decal_texture(), m_displacement_texture(), m_normal_texture(),
-			m_extra_parameters{} {}
+			m_extra_parameters{} {
+			UpdateTransparency();
+		}
 		
 		/**
 		 Constructs a material from the given material.

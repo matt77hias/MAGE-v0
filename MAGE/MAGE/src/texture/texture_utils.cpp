013605aeaf4ad6ff5116bf6785921447519f02b3
REDDRAGON <matthias.moulin@gmail.com>
2017-07-27 17:14:55 +0200

    Refactor

--- MAGE/MAGE/src/texture/texture_utils.cpp
+++ MAGE/MAGE/src/texture/texture_utils.cpp
@@ -5,6 +5,7 @@
 
 #include "memory\memory.hpp"
 #include "texture\texture_utils.hpp"
+#include "rendering\rendering_utils.hpp"
 #include "logging\error.hpp"
 #include "logging\exception.hpp"
 
@@ -15,19 +16,19 @@
 //-----------------------------------------------------------------------------
 namespace mage {
 
-	const XMVECTOR GetTexture2DSize(ID3D11ShaderResourceView *texture) {
-		Assert(texture);
+	const XMVECTOR GetTexture2DSize(ID3D11ShaderResourceView *texture_srv) {
+		Assert(texture_srv);
 
 		ComPtr< ID3D11Resource > resource;
-		texture->GetResource(&resource);
+		texture_srv->GetResource(&resource);
 
-		ComPtr< ID3D11Texture2D > texture2D;
-		const HRESULT result_texture_2d = resource.As(&texture2D);
-		if (FAILED(result_texture_2d)) {
+		ComPtr< ID3D11Texture2D > texture;
+		const HRESULT result_texture = resource.As(&texture);
+		if (FAILED(result_texture)) {
 			throw FormattedException("Conversion of ID3D11Resource to Texture2D failed.");
 		}
 
-		return GetTexture2DSize(texture2D.Get());
+		return GetTexture2DSize(texture.Get());
 	}
 
 	const XMVECTOR GetTexture2DSize(ID3D11Texture2D *texture) noexcept {
@@ -37,9 +38,75 @@ namespace mage {
 		D3D11_TEXTURE2D_DESC desc;
 		texture->GetDesc(&desc);
 
-		const XMVECTOR width = XMLoadInt(&desc.Width);
+		const XMVECTOR width  = XMLoadInt(&desc.Width);
 		const XMVECTOR height = XMLoadInt(&desc.Height);
-		const XMVECTOR size = XMVectorMergeXY(width, height);
+		const XMVECTOR size   = XMVectorMergeXY(width, height);
 		return XMConvertVectorUIntToFloat(size, 0);
+	}
+
+	bool HasAlpha(ID3D11ShaderResourceView *texture_srv) noexcept {
+		Assert(texture_srv);
+
+		ComPtr< ID3D11Resource > resource;
+		texture_srv->GetResource(&resource);
+
+		D3D11_RESOURCE_DIMENSION dimension;
+		resource->GetType(&dimension);
+		switch (dimension) {
+		
+		case D3D11_RESOURCE_DIMENSION_TEXTURE1D: {
+			ComPtr< ID3D11Texture1D > texture;
+			const HRESULT result_texture = resource.As(&texture);
+			Assert(SUCCEEDED(result_texture));
+			return HasAlpha(texture.Get());
+		}
+
+		case D3D11_RESOURCE_DIMENSION_TEXTURE2D: {
+			ComPtr< ID3D11Texture2D > texture;
+			const HRESULT result_texture = resource.As(&texture);
+			Assert(SUCCEEDED(result_texture));
+			return HasAlpha(texture.Get());
+		}
+
+		case D3D11_RESOURCE_DIMENSION_TEXTURE3D: {
+			ComPtr< ID3D11Texture3D > texture;
+			const HRESULT result_texture = resource.As(&texture);
+			Assert(SUCCEEDED(result_texture));
+			return HasAlpha(texture.Get());
+		}
+
+		default:
+			return false;
+		}
+	}
+
+	bool HasAlpha(ID3D11Texture1D *texture) noexcept {
+		Assert(texture);
+
+		// Query the texture size.
+		D3D11_TEXTURE1D_DESC desc;
+		texture->GetDesc(&desc);
+
+		return HasAlpha(desc.Format);
+	}
+
+	bool HasAlpha(ID3D11Texture2D *texture) noexcept {
+		Assert(texture);
+
+		// Query the texture size.
+		D3D11_TEXTURE2D_DESC desc;
+		texture->GetDesc(&desc);
+
+		return HasAlpha(desc.Format);
+	}
+
+	bool HasAlpha(ID3D11Texture3D *texture) noexcept {
+		Assert(texture);
+
+		// Query the texture size.
+		D3D11_TEXTURE3D_DESC desc;
+		texture->GetDesc(&desc);
+
+		return HasAlpha(desc.Format);
 	}
 }
--- MAGE/MAGE/src/texture/texture_utils.hpp
+++ MAGE/MAGE/src/texture/texture_utils.hpp
@@ -18,7 +18,7 @@ namespace mage {
 	/**
 	 Returns the size of the given 2D texture.
 
-	 @pre			@a texture_srv is not equal to @c nullptr.		
+	 @pre			@a texture_srv is not equal to @c nullptr.
 	 @param[in]		texture_srv
 					A pointer to the (texture) shader resource view.
 	 @return		The size of the given 2D texture as a
@@ -39,4 +39,48 @@ namespace mage {
 					@c XMVECTOR (width, height, width, height).
 	 */
 	const XMVECTOR GetTexture2DSize(ID3D11Texture2D *texture) noexcept;
+
+	/**
+	 Checks whether the given texture contains an alpha component.
+
+	 @pre			@a texture_srv is not equal to @c nullptr.
+	 @param[in]		texture_srv
+					A pointer to the (texture) shader resource view.
+	 @return		@c true if and only if the given texture contains
+					an alpha component.
+	 */
+	bool HasAlpha(ID3D11ShaderResourceView *texture_srv) noexcept;
+
+	/**
+	 Checks whether the given 1D texture contains an alpha component.
+
+	 @pre			@a texture is not equal to @c nullptr.
+	 @param[in]		texture
+					A pointer to the 1D texture.
+	 @return		@c true if and only if the given 1D texture contains
+					an alpha component.
+	 */
+	bool HasAlpha(ID3D11Texture1D *texture) noexcept;
+
+	/**
+	 Checks whether the given 2D texture contains an alpha component.
+
+	 @pre			@a texture is not equal to @c nullptr.
+	 @param[in]		texture
+					A pointer to the 2D texture.
+	 @return		@c true if and only if the given 2D texture contains
+ 					an alpha component.
+	 */
+	bool HasAlpha(ID3D11Texture2D *texture) noexcept;
+
+	/**
+	 Checks whether the given 3D texture contains an alpha component.
+
+	 @pre			@a texture is not equal to @c nullptr.
+	 @param[in]		texture
+					A pointer to the 3D texture.
+	 @return		@c true if and only if the given 3D texture contains
+					an alpha component.
+	 */
+	bool HasAlpha(ID3D11Texture3D *texture) noexcept;
 }

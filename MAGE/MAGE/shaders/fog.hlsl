a99090632f632224f5b703898447c63b2a7a940f
REDDRAGON <matthias.moulin@gmail.com>
2017-07-28 11:34:04 +0200

    Removed fog.hlsl

--- MAGE/MAGE/shaders/fog.hlsl
+++
@@ -1,9 +0,0 @@
-//TODO
-
-float ComputeFogFactor(float4 p) {
-	return saturate(dot(p, fog_vector));
-}
-
-void ApplyFog(inout float4 color, float fog_factor) {
-	color.rgb = lerp(color.rgb, fog_color * color.a, fog_factor);
-}

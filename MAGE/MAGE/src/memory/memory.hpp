9b30a4cd32d2ea49818d68cf3d117b7261763f0d
REDDRAGON <matthias.moulin@gmail.com>
2017-08-11 14:56:07 +0200

    Updated SharedPtr and UniquePtr usage

--- MAGE/MAGE/src/memory/memory.hpp
+++ MAGE/MAGE/src/memory/memory.hpp
@@ -53,6 +53,7 @@ namespace mage {
 	//-------------------------------------------------------------------------
 	// ComPtr
 	//-------------------------------------------------------------------------
+#pragma region
 
 	/**
 	 A class of smart pointers for managing shared-ownership COM resources.
@@ -63,9 +64,12 @@ namespace mage {
 	template < typename T >
 	using ComPtr = Microsoft::WRL::ComPtr< T >;
 
+#pragma endregion
+
 	//-------------------------------------------------------------------------
 	// SharedPointer
 	//-------------------------------------------------------------------------
+#pragma region
 
 	/**
 	 A class of smart pointers for managing shared-ownership memory resources.
@@ -76,9 +80,38 @@ namespace mage {
 	template < typename T >
 	using SharedPtr = std::shared_ptr< T >;
 
+	/**
+	 Constructs an object of type T. 
+	 No custom allocator is used for constructing the object of type T.
+
+	 @tparam		T
+					The type.
+	 @tparam		ConstructorArgsT
+					The constructor argument types of the camera.
+	 @return		A shared pointer to the constructed object of type T.
+	 */
+	template< typename T, typename... ConstructorArgsT >
+	SharedPtr< T > MakeShared(ConstructorArgsT&&... args);
+
+	/**
+	 Constructs an object of type T.
+	 The custom allocator is used for constructing the object of type T.
+
+	 @tparam		T
+					The type.
+	 @tparam		ConstructorArgsT
+					The constructor argument types of the camera.
+	 @return		A shared pointer to the constructed object of type T.
+	 */
+	template< typename T, typename... ConstructorArgsT >
+	SharedPtr< T > MakeAllocatedShared(ConstructorArgsT&&... args);
+
+#pragma endregion
+
 	//-------------------------------------------------------------------------
 	// WeakPointer
 	//-------------------------------------------------------------------------
+#pragma region
 
 	/**
 	 A class of smart pointers for referencing but not managing
@@ -90,9 +123,12 @@ namespace mage {
 	template < typename T >
 	using WeakPtr = std::weak_ptr< T >;
 
+#pragma endregion
+
 	//-------------------------------------------------------------------------
 	// UniquePointer
 	//-------------------------------------------------------------------------
+#pragma region
 
 	/**
 	 A class of smart pointers for managing exclusive-ownership memory resources.
@@ -100,9 +136,51 @@ namespace mage {
 	 @tparam		T
 					The memory resource type.
 	 */
-	template < typename T >
-	using UniquePtr = std::unique_ptr< T >;
+	template < typename T, typename DeleterT = std::default_delete< T > >
+	using UniquePtr = std::unique_ptr< T, DeleterT >;
 
+	/**
+	 Constructs a non-array type T. This overload only participates 
+	 in overload resolution if T is not an array type.
+
+	 @tparam		T
+					The type.
+	 @tparam		ConstructorArgsT
+					The constructor argument types of the camera.
+	 @return		A unique pointer to the constructed non-array type T.
+	 */
+	template< typename T, typename... ConstructorArgsT >
+	typename std::enable_if< !std::is_array< T >::value, 
+		UniquePtr< T > >::type MakeUnique(ConstructorArgsT&&... args);
+	
+	/**
+	 Constructs an array of unknown bound T. This overload only participates 
+	 in overload resolution if T is an array of unknown bound.
+
+	 @tparam		T
+					The type.
+	 @param[in]		count
+					The number of elements of the array to construct.
+	 @return		A unique pointer to the constructed array of unknown bound T.
+	 */
+	template< typename T >
+	typename std::enable_if< std::is_array< T >::value && std::extent< T >::value == 0,
+		UniquePtr< T > >::type MakeUnique(size_t count);
+
+	/**
+	 Constructs an array of known bound T. This overload only participates
+	 in overload resolution if T is an array of known bound.
+
+	 @tparam		T
+					The type.
+	 @param[in]		count
+					The number of elements of the array to construct.
+	 @return		A unique pointer to the constructed array of known bound T.
+	 */
+	template< typename T >
+	typename std::enable_if< std::extent< T >::value != 0,
+		UniquePtr< T > >::type MakeUnique(size_t count) = delete;
+	
 	/**
 	 Creates a unique pointer whose stored pointer is obtained
 	 by statically casting the stored pointer of the given unique pointer.
@@ -116,9 +194,7 @@ namespace mage {
 	 @return		The moved unique pointer.
 	 */
 	template< typename T, typename U >
-	inline UniquePtr< T > static_pointer_cast(UniquePtr< U > &&ptr) noexcept {
-		return UniquePtr< T >(static_cast< T * >(ptr.release()));
-	}
+	inline UniquePtr< T > static_pointer_cast(UniquePtr< U > &&ptr) noexcept;
 
 	/**
 	 Creates a unique pointer whose stored pointer is obtained
@@ -133,17 +209,7 @@ namespace mage {
 	 @return		The moved unique pointer.
 	 */
 	template< typename T, typename U >
-	inline UniquePtr< T > dynamic_pointer_cast(UniquePtr< U > &&ptr) noexcept {
-		U * const stored_ptr = ptr.release();
-		T * const converted_stored_ptr = dynamic_cast< T * >(stored_ptr);
-		if (converted_stored_ptr) {
-			return UniquePtr< T >(converted_stored_ptr);
-		}
-		else {
-			ptr.reset(stored_ptr);
-			return UniquePtr< T >(nullptr);
-		}
-	}
+	inline UniquePtr< T > dynamic_pointer_cast(UniquePtr< U > &&ptr) noexcept;
 
 	/**
 	 Creates a unique pointer whose stored pointer is obtained
@@ -158,9 +224,7 @@ namespace mage {
 	 @return		The moved unique pointer.
 	 */
 	template< typename T, typename U >
-	inline UniquePtr< T > const_pointer_cast(UniquePtr< U > &&ptr) noexcept {
-		return UniquePtr< T >(const_cast< T * >(ptr.release()));
-	}
+	inline UniquePtr< T > const_pointer_cast(UniquePtr< U > &&ptr) noexcept;
 
 	/**
 	 Creates a unique pointer whose stored pointer is obtained
@@ -175,13 +239,14 @@ namespace mage {
 	 @return		The moved unique pointer.
 	 */
 	template< typename T, typename U >
-	inline UniquePtr< T > reinterpret_pointer_cast(UniquePtr< U > &&ptr) noexcept {
-		return UniquePtr< T >(reinterpret_cast< T * >(ptr.release()));
-	}
+	inline UniquePtr< T > reinterpret_pointer_cast(UniquePtr< U > &&ptr) noexcept;
+
+#pragma endregion
 
 	//-------------------------------------------------------------------------
 	// UniqueHandle and SharedHandle
 	//-------------------------------------------------------------------------
+#pragma region
 
 	/**
 	 Destructs the given handle.
@@ -214,7 +279,7 @@ namespace mage {
 	/**
 	 A class of smart pointers for managing exclusive-ownership handle resources.
 	 */
-	using UniqueHandle = std::unique_ptr< void, HandleCloser >;
+	using UniqueHandle = UniquePtr< void, HandleCloser >;
 
 	/**
 	 A class of smart pointers for managing shared-ownership handle resources.
@@ -246,9 +311,12 @@ namespace mage {
 		return SharedHandle(SafeHandle(handle), DestructHandle);
 	}
 
+#pragma endregion
+
 	//-------------------------------------------------------------------------
 	// UniqueFileStream
 	//-------------------------------------------------------------------------
+#pragma region
 
 	/**
 	 A struct of file stream destructors (i.e. for closing file streams).
@@ -271,5 +339,16 @@ namespace mage {
 	/**
 	 A class of smart pointers for managing exclusive-ownership file streams.
 	 */
-	using UniqueFileStream = std::unique_ptr< FILE, FileStreamCloser >;
+	using UniqueFileStream = UniquePtr< FILE, FileStreamCloser >;
+
+#pragma endregion
 }
+
+//-----------------------------------------------------------------------------
+// Engine Includes
+//-----------------------------------------------------------------------------
+#pragma region
+
+#include "memory\memory.tpp"
+
+#pragma endregion
---
+++ MAGE/MAGE/src/memory/memory.tpp
@@ -0,0 +1,65 @@
+#pragma once
+
+//-----------------------------------------------------------------------------
+// Engine Definitions
+//-----------------------------------------------------------------------------
+namespace mage {
+
+	//-------------------------------------------------------------------------
+	// SharedPointer
+	//-------------------------------------------------------------------------
+
+	template< typename T, typename... ConstructorArgsT >
+	inline SharedPtr< T > MakeShared(ConstructorArgsT&&... args) {
+		return std::make_shared< T >(std::forward< ConstructorArgsT >(args)...);
+	}
+
+	template< typename T, typename... ConstructorArgsT >
+	inline SharedPtr< T > MakeAllocatedShared(ConstructorArgsT&&... args) {
+		return SharedPtr< T >(new T(std::forward< ConstructorArgsT >(args)...));
+	}
+
+	//-------------------------------------------------------------------------
+	// UniquePointer
+	//-------------------------------------------------------------------------
+
+	template< typename T, typename... ConstructorArgsT >
+	inline typename std::enable_if< !std::is_array< T >::value,
+		UniquePtr< T > >::type MakeUnique(ConstructorArgsT&&... args) {
+		return std::make_unique< T >(std::forward< ConstructorArgsT >(args)...);
+	}
+
+	template< typename T >
+	inline typename std::enable_if< std::is_array< T >::value && std::extent< T >::value == 0,
+		UniquePtr< T > >::type MakeUnique(size_t count) {
+		return std::make_unique< T >(count);
+	}
+
+	template< typename T, typename U >
+	inline UniquePtr< T > static_pointer_cast(UniquePtr< U > &&ptr) noexcept {
+		return UniquePtr< T >(static_cast< T * >(ptr.release()));
+	}
+
+	template< typename T, typename U >
+	inline UniquePtr< T > dynamic_pointer_cast(UniquePtr< U > &&ptr) noexcept {
+		U * const stored_ptr = ptr.release();
+		T * const converted_stored_ptr = dynamic_cast< T * >(stored_ptr);
+		if (converted_stored_ptr) {
+			return UniquePtr< T >(converted_stored_ptr);
+		}
+		else {
+			ptr.reset(stored_ptr);
+			return UniquePtr< T >(nullptr);
+		}
+	}
+
+	template< typename T, typename U >
+	inline UniquePtr< T > const_pointer_cast(UniquePtr< U > &&ptr) noexcept {
+		return UniquePtr< T >(const_cast< T * >(ptr.release()));
+	}
+
+	template< typename T, typename U >
+	inline UniquePtr< T > reinterpret_pointer_cast(UniquePtr< U > &&ptr) noexcept {
+		return UniquePtr< T >(reinterpret_cast< T * >(ptr.release()));
+	}
+}

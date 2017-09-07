7d0804cf814d4ff97c120d0bd7acbaf28a94c16f
ALIEN <matthias.moulin@gmail.com>
2017-09-07 17:31:04 +0200

    Added binding header

--- MAGE/MAGE/shaders/hlsl.hpp
+++ MAGE/MAGE/shaders/hlsl.hpp
@@ -1,7 +1,29 @@
-#ifndef MAGE_HEADER_CONFIGURATION
-#define MAGE_HEADER_CONFIGURATION
+#ifndef MAGE_HEADER_HLSL
+#define MAGE_HEADER_HLSL
 
 #define MAGE_NVIDIA_WARP_SIZE   32
 #define MAGE_AMD_WAFEFRONT_SIZE 64
+#define DEFAULT_GROUP_SIZE      16
 
-#endif // MAGE_HEADER_CONFIGURATION
+#define B_REGISTER(x)	(b##x)
+#define T_REGISTER(x)	(t##x)
+#define S_REGISTER(x)	(s##x)
+#define U_REGISTER(x)	(u##x)
+
+#define SLOT_CBUFFER_COLOR          0
+#define SLOT_CBUFFER_LIGHT          0
+#define SLOT_CBUFFER_PER_FRAME      1
+#define SLOT_CBUFFER_PER_DRAW       2
+
+#define SLOT_SRV_SPRITE             0
+#define SLOT_SRV_DIRECTIONAL_LIGHTS 0
+#define SLOT_SRV_OMNI_LIGHTS        1
+#define SLOT_SRV_SPOT_LIGHTS        2
+#define SLOT_SRV_DIFFUSE            3
+#define SLOT_SRV_SPECULAR           4
+#define SLOT_SRV_NORMAL             5
+#define SLOT_SRV_DEPTH              6
+
+#define SLOT_UAV_IMAGE              0
+
+#endif // MAGE_HEADER_HLSL
--- MAGE/MAGE/shaders/miscellaneous/distance_PS.hlsl
+++ MAGE/MAGE/shaders/miscellaneous/distance_PS.hlsl
@@ -6,7 +6,7 @@
 //-----------------------------------------------------------------------------
 // Pixel Shader
 //-----------------------------------------------------------------------------
-float4 PS(PSInputPositionNormalTexture input) : SV_Target{
+float4 PS(PSInputPositionNormalTexture input) : SV_Target {
 	const float c = 1.0f - saturate(length(input.p_view) / 5.0f);
 	return float4(c, c, c, 1.0f);
 }

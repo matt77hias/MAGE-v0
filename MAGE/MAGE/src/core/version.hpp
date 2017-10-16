c8fa8292413a7bb5a9b0f229c04444b1aacc2313
Matthias Moulin <matthias.moulin@gmail.com>
2017-10-16 19:24:10 +0200

    Update version.hpp

--- MAGE/MAGE/src/core/version.hpp
+++ MAGE/MAGE/src/core/version.hpp
@@ -17,7 +17,7 @@
 // The version numbers (major/minor/patch) must be macros, allowing the use of 
 // these numbers in preprocessor directives (e.g. if).
 #define MAGE_VERSION_MAJOR 0
-#define MAGE_VERSION_MINOR 80
+#define MAGE_VERSION_MINOR 81
 #define MAGE_VERSION_PATCH 0
 
 #define MAGE_QUOTE(S) #S

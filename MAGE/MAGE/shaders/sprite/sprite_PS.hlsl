00352c61a29fb81eaa206f017392a088a4e7cadf
REDDRAGON <matthias.moulin@gmail.com>
2017-10-17 19:33:28 +0200

    Added explanation why no explicit gamma correction is needed for sprites

--- MAGE/MAGE/shaders/sprite/sprite_PS.hlsl
+++ MAGE/MAGE/shaders/sprite/sprite_PS.hlsl
@@ -12,5 +12,6 @@ TEXTURE_2D(g_sprite, float4, SLOT_SRV_SPRITE);
 // Pixel Shader
 //-----------------------------------------------------------------------------
 float4 PS(PSInputColorTexture input) : SV_Target {
+	// Gr := (C1 * C2)^(1/gamma) = C1^(1/gamma) C2^(1/gamma)
 	return g_sprite.Sample(g_linear_wrap_sampler, input.tex) * input.color;
 }

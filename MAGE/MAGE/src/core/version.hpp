de7fa057ae5e0f359861fe92700da0bebefca868
REDDRAGON <matthias.moulin@gmail.com>
2017-11-03 11:03:19 +0100

    Updated version

--- MAGE/MAGE/src/core/version.hpp
+++ MAGE/MAGE/src/core/version.hpp
@@ -17,7 +17,7 @@
 // The version numbers (major/minor/patch) must be macros, allowing the use of 
 // these numbers in preprocessor directives (e.g. if).
 #define MAGE_VERSION_MAJOR 0
-#define MAGE_VERSION_MINOR 101
+#define MAGE_VERSION_MINOR 102
 #define MAGE_VERSION_PATCH 0
 
 #define MAGE_QUOTE(S) #S

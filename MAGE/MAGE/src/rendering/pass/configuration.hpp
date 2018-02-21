af4763a6ff47284bdbfebeb71c64174f5eb7b833
ALIEN <matthias.moulin@gmail.com>
2018-02-21 11:31:01 +0100

    Refactor

--- MAGE/MAGE/src/rendering/pass/configuration.hpp
+++ MAGE/MAGE/src/rendering/pass/configuration.hpp
@@ -10,41 +10,64 @@ namespace mage {
 
 	 This contains:
 	 @c None,
-	 @c Forward, 
+	 @c Forward,
 	 @c Deferred,
-	 @c Solid, 
-	 @c BaseColor,
-	 @c BaseColorCoefficient,
-	 @c BaseColorTexture,
-	 @c Material,
-	 @c MaterialCoefficient,
-	 @c MaterialTexture,
-	 @c NormalTexture,
-	 @c UVTexture,
-	 @c Distance,
-	 @c ShadingNormal, and
-	 @c TSNMShadingNormal.
+	 @c Solid,            
+	 @c Voxelization,
+	 @c GBuffer_Depth,
+	 @c GBuffer_BaseColor,
+	 @c GBuffer_Material,
+	 @c GBuffer_Normal,
+	 @c FalseColor_BaseColor,
+	 @c FalseColor_BaseColorCoefficient,
+	 @c FalseColor_BaseColorTexture,
+	 @c FalseColor_Material,
+	 @c FalseColor_MaterialCoefficient,
+	 @c FalseColor_MaterialTexture,
+	 @c FalseColor_Roughness,
+	 @c FalseColor_RoughnessCoefficient,
+	 @c FalseColor_RoughnessTexture,
+	 @c FalseColor_Metalness,
+	 @c FalseColor_MetalnessCoefficient,
+	 @c FalseColor_MetalnessTexture,
+	 @c FalseColor_ShadingNormal,
+	 @c FalseColor_TSNMShadingNormal,
+	 @c FalseColor_UVTexture, and
+	 @c FalseColor_Distance.
 	 */
 	enum class RenderMode : U8 {
 		// Rendering
-		None                 = 0,  // No shading.
-		Forward              = 1,  // Forward shading.
-		Deferred             = 2,  // Deferred shading.
-		Solid                = 3,  // Solid shading.             
+		None = 0,
+		Forward,
+		Deferred,
+		Solid,            
 		
-		// Material
-		BaseColor            = 4,  // Base Color (coefficient + texture).
-		BaseColorCoefficient = 5,  // Base Color coefficient.
-		BaseColorTexture     = 6,  // Base Color texture.
-		Material             = 7,  // Material (coefficient + texture).
-		MaterialCoefficient  = 8,  // Material coefficient.
-		MaterialTexture      = 9,  // Material texture.
-		NormalTexture        = 10, // Normal texture.
-		
-		UVTexture            = 11, // UV parametrization texture.
-		Distance             = 12, // The distance towards the eye.
-		ShadingNormal        = 13, // The shading normal.
-		TSNMShadingNormal    = 14  // The TSNM shading normal.
+		// Voxelization visualization
+		Voxelization,
+
+		// GBuffer visualization
+		GBuffer_Depth,
+		GBuffer_BaseColor,
+		GBuffer_Material,
+		GBuffer_Normal,
+
+		// False color visualization
+		FalseColor_BaseColor,
+		FalseColor_BaseColorCoefficient,
+		FalseColor_BaseColorTexture,
+		FalseColor_Material,
+		FalseColor_MaterialCoefficient,
+		FalseColor_MaterialTexture,
+		FalseColor_Roughness,
+		FalseColor_RoughnessCoefficient,
+		FalseColor_RoughnessTexture,
+		FalseColor_Metalness,
+		FalseColor_MetalnessCoefficient,
+		FalseColor_MetalnessTexture,
+		FalseColor_ShadingNormal,
+		FalseColor_TSNMShadingNormal,
+		FalseColor_UVTexture,
+		FalseColor_Distance
 	};
 
 	/**
@@ -56,8 +79,8 @@ namespace mage {
 	 @c AABB.
 	 */
 	enum class RenderLayer : U32 {
-		None      = 0, // No layer.
-		Wireframe = 1, // Wirframe layer.
-		AABB      = 2, // AABB layer.
+		None = 0,  // No layer.
+		Wireframe, // Wirframe layer.
+		AABB,      // AABB layer.
 	};
 }

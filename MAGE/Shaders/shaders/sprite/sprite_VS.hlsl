3c35714c69ca220395dfda228d8ab0f9e41216d0
REDDRAGON <matthias.moulin@gmail.com>
2018-04-20 11:16:39 +0200

    Fixed: removed minus one half (GPU)

--- MAGE/Shaders/shaders/sprite/sprite_VS.hlsl
+++ MAGE/Shaders/shaders/sprite/sprite_VS.hlsl
@@ -11,7 +11,7 @@ PSInputColorTexture VS(VSInputPositionColorTexture input) {
 	
 	// .x: [0, g_display_resolution.x] -> [-1,1]
 	// .y: [0, g_display_resolution.y] -> [1,-1]
-	output.p.xy  = float2(2.0f, -2.0f) * input.p.xy / g_display_resolution 
+	output.p.xy  = float2(2.0f, -2.0f) * input.p.xy * g_display_inv_resolution
 		         + float2(-1.0f, 1.0f);
 	output.p.zw  = float2(input.p.z, 1.0f);
 	output.color = input.color;

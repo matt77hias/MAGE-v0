388e8c8532ff3b05a71543e43285b4f496e2783a
REDDRAGON <matthias.moulin@gmail.com>
2018-04-27 15:18:43 +0200

    FXC bug workaround

--- MAGE/Shaders/shaders/sprite/sprite_VS.hlsl
+++ MAGE/Shaders/shaders/sprite/sprite_VS.hlsl
@@ -9,7 +9,7 @@
 PSInputColorTexture VS(VSInputPositionColorTexture input) {
 	PSInputColorTexture output;
 	output.p.xy  = UVtoNDC(DisplayToUV(input.p.xy));
-	output.p.zw  = { input.p.z, 1.0f };
+	output.p.zw  = float2(input.p.z, 1.0f);
 	output.color = input.color;
 	output.tex   = input.tex;
 	return output;

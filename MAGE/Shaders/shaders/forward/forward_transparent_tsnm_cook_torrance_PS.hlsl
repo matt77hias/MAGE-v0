04c30b975a2b12068c6fc60b11293b04cb33094d
matt77hias <matthias.moulin@gmail.com>
2018-08-14 11:19:25 +0200

    Refactor

--- MAGE/Shaders/shaders/forward/forward_transparent_tsnm_cook_torrance_PS.hlsl
+++ MAGE/Shaders/shaders/forward/forward_transparent_tsnm_cook_torrance_PS.hlsl
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------------
 // Engine Includes
 //-----------------------------------------------------------------------------
-#define BRDF_FUNCTION CookTorranceBRDF
+#define BRDF_COOK_TORRANCE
 #define DISABLE_VCT
 #define ENABLE_TRANSPARENCY
 #include "forward\forward.hlsli"
--- MAGE/Shaders/shaders/forward/forward_transparent_tsnm_lambertian_PS.hlsl
+++ MAGE/Shaders/shaders/forward/forward_transparent_tsnm_lambertian_PS.hlsl
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------------
 // Engine Includes
 //-----------------------------------------------------------------------------
-#define BRDF_FUNCTION LambertianBRDF
+#define BRDF_LAMBERTIAN
 #define DISABLE_VCT
 #define ENABLE_TRANSPARENCY
 #include "forward\forward.hlsli"

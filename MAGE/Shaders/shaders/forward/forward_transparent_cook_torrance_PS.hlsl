a50e91507bd403c5615073cc88f35f70053d9600
matt77hias <matthias.moulin@gmail.com>
2018-08-14 11:17:38 +0200

    Refactor

--- MAGE/Shaders/shaders/forward/forward_transparent_cook_torrance_PS.hlsl
+++ MAGE/Shaders/shaders/forward/forward_transparent_cook_torrance_PS.hlsl
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------------
 // Engine Includes
 //-----------------------------------------------------------------------------
-#define BRDF_FUNCTION CookTorranceBRDF
+#define BRDF_COOK_TORRANCE
 #define DISABLE_TSNM
 #define DISABLE_VCT
 #define ENABLE_TRANSPARENCY

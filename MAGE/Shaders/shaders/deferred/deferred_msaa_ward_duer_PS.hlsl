fae7d741076f6942f5c137f60d381ab3d3573af8
matt77hias <matthias.moulin@gmail.com>
2018-08-14 11:06:06 +0200

    Refactor

--- MAGE/Shaders/shaders/deferred/deferred_msaa_ward_duer_PS.hlsl
+++ MAGE/Shaders/shaders/deferred/deferred_msaa_ward_duer_PS.hlsl
@@ -1,10 +1,7 @@
 //-----------------------------------------------------------------------------
 // Engine Includes
 //-----------------------------------------------------------------------------
-#define BRDF_FUNCTION   CookTorranceBRDF
-#define BRDF_D_FUNCTION D_WardDuer
-#define BRDF_F_FUNCTION F_None
-#define BRDF_V_FUNCTION V_Ward
+#define BRDF_WARD_DUER
 #define DISABLE_VCT
 #define MSAA
 #include "deferred\deferred.hlsli"
--- MAGE/Shaders/shaders/deferred/deferred_vct_ward_duer_CS.hlsl
+++ MAGE/Shaders/shaders/deferred/deferred_vct_ward_duer_CS.hlsl
@@ -1,8 +1,5 @@
 //-----------------------------------------------------------------------------
 // Engine Includes
 //-----------------------------------------------------------------------------
-#define BRDF_FUNCTION   CookTorranceBRDF
-#define BRDF_D_FUNCTION D_WardDuer
-#define BRDF_F_FUNCTION F_None
-#define BRDF_V_FUNCTION V_Ward
+#define BRDF_WARD_DUER
 #include "deferred\deferred.hlsli"

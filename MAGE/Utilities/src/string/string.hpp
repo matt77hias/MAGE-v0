eae7c65feacfce0c023654b75da662a28870d382
REDDRAGON <matthias.moulin@gmail.com>
2018-09-08 15:38:33 +0200

    Refactor

--- MAGE/Utilities/src/string/string.hpp
+++ MAGE/Utilities/src/string/string.hpp
@@ -10,17 +10,22 @@
 
 #include <gsl/pointers>
 #include <gsl/string_span>
+
+#pragma endregion
+
+//-----------------------------------------------------------------------------
+// Type Declarations and Definitions
+//-----------------------------------------------------------------------------
 namespace mage {
+
 	using zstring        = gsl::zstring<>;
 	using wzstring       = gsl::wzstring<>;
 	using const_zstring  = gsl::czstring<>;
 	using const_wzstring = gsl::cwzstring<>;
 
 	/**
-	 A class of not-null values.
+	 A type for representing not-null values.
 	 */
 	template< typename T >
 	using NotNull = gsl::not_null< T >;
 }
-
-#pragma endregion

b101d2b0bfe7594fb6d236422a8b59e5b67de3fe
REDDRAGON <matthias.moulin@gmail.com>
2018-09-16 12:49:52 +0200

    Refactor: added better partial template specialization for Array formatting

--- MAGE/Utilities/src/string/formats.hpp
+++ MAGE/Utilities/src/string/formats.hpp
@@ -23,8 +23,8 @@
 //-----------------------------------------------------------------------------
 namespace fmt {
 
-	template<>
-	struct formatter< mage::F32x2 > {
+	template< typename T, size_t A >
+	struct formatter< mage::Array< T, 2u, A > > {
 
 		template< typename ParseContextT >
 		constexpr auto parse(ParseContextT &ctx) {
@@ -32,14 +32,14 @@ namespace fmt {
 		}
 
 		template< typename FormatContextT >
-		auto format(const mage::F32x2& a, FormatContextT &ctx) {
+		auto format(const mage::Array< T, 2u, A >& a, FormatContextT &ctx) {
 			const auto [x, y] = a;
 			return format_to(ctx.begin(), "{} {}", x, y);
 		}
 	};
 
-	template<>
-	struct formatter< mage::F32x3 > {
+	template< typename T, size_t A >
+	struct formatter< mage::Array< T, 3u, A > > {
 
 		template< typename ParseContextT >
 		constexpr auto parse(ParseContextT &ctx) {
@@ -47,14 +47,14 @@ namespace fmt {
 		}
 
 		template< typename FormatContextT >
-		auto format(const mage::F32x3& a, FormatContextT &ctx) {
+		auto format(const mage::Array< T, 3u, A >& a, FormatContextT &ctx) {
 			const auto [x, y, z] = a;
 			return format_to(ctx.begin(), "{} {} {}", x, y, z);
 		}
 	};
 
-	template<>
-	struct formatter< mage::F32x4 > {
+	template< typename T, size_t A >
+	struct formatter< mage::Array< T, 4u, A > > {
 
 		template< typename ParseContextT >
 		constexpr auto parse(ParseContextT &ctx) {
@@ -62,412 +62,7 @@ namespace fmt {
 		}
 
 		template< typename FormatContextT >
-		auto format(const mage::F32x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::F64x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::F64x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::F64x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::F64x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::F64x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::F32x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S8x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin(); 
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S8x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S8x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S8x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S8x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S8x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S16x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S16x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S16x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S16x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S16x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S16x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S32x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S32x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S32x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S32x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S32x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S32x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S64x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S64x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S64x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S64x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::S64x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::S32x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U8x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin(); 
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U8x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U8x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U8x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U8x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U8x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U16x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U16x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U16x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U16x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U16x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U16x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U32x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U32x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U32x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U32x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U32x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U32x4& a, FormatContextT &ctx) {
-			const auto [x, y, z, w] = a;
-			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U64x2 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U64x2& a, FormatContextT &ctx) {
-			const auto [x, y] = a;
-			return format_to(ctx.begin(), "{} {}", x, y);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U64x3 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U64x3& a, FormatContextT &ctx) {
-			const auto [x, y, z] = a;
-			return format_to(ctx.begin(), "{} {} {}", x, y, z);
-		}
-	};
-
-	template<>
-	struct formatter< mage::U64x4 > {
-
-		template< typename ParseContextT >
-		constexpr auto parse(ParseContextT &ctx) {
-			return ctx.begin();
-		}
-
-		template< typename FormatContextT >
-		auto format(const mage::U32x4& a, FormatContextT &ctx) {
+		auto format(const mage::Array< T, 4u, A >& a, FormatContextT &ctx) {
 			const auto [x, y, z, w] = a;
 			return format_to(ctx.begin(), "{} {} {} {}", x, y, z, w);
 		}

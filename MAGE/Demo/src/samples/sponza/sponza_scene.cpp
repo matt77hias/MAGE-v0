5efc8d52ba256155284bf01a55b8d30fd4737de1
REDDRAGON <matthias.moulin@gmail.com>
2018-09-20 21:38:20 +0200

    sponza.mdl

--- MAGE/Demo/src/samples/sponza/sponza_scene.cpp
+++ MAGE/Demo/src/samples/sponza/sponza_scene.cpp
@@ -56,7 +56,7 @@ namespace mage {
 				L"assets/models/plane/plane.obj", mesh_desc);
 		const auto sponza_model_desc
 			= rendering_factory.GetOrCreate< ModelDescriptor >(
-				L"assets/models/sponza/sponza.obj",    mesh_desc);
+				L"assets/models/sponza/sponza.mdl",    mesh_desc);
 		const auto tree_model_desc_tree
 			= rendering_factory.GetOrCreate< ModelDescriptor >(
 				L"assets/models/tree/tree1a_lod0.mdl", mesh_desc);
